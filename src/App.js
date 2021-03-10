10faebcb5e48206dd18fd04b36c42c8c13826a73
Pedro Manuel Jurado Moreno <mpedrojurado@gmail.com>
2021-03-09 21:40:40 -0500

    subiendo multiple css react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { TemaData } from './dataLocal';
+import github from './assets/img/github.png';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      listaTema: [], // arreglo para guardar la lista
+
+    }
+  }
+
+  componentDidMount() {
+    this.setState({
+      listaTema: TemaData, // lista == dataLocal
+    })
+    console.log(TemaData) // mostrando por consola la dataLocal
+  }
+
+  dibujarTema(dataTema) {
+    return (
+      <table className="table table-striped table-hover overflow-auto">
+      <thead className="thead-dark">
+          <tr className="text-center">
+              <th>ID</th>
+              <th>TEMA</th>
+              <th>CATEGORIA</th>
+          </tr>
+      </thead>
+      <tbody>
+          {dataTema.map(itemTema =>
+              <tr key={itemTema.id} className="text-center">
+                  <td>{itemTema.id}</td>
+                  <td>{itemTema.tema}</td>
+                  <td> <span className=
+                        {` 
+                                ${itemTema.categoria === "React" ? 'badge badge-pill badge-info' : ''}
+                                ${itemTema.categoria === "Angular" ? 'badge badge-pill badge-danger' : ''}
+                                ${itemTema.categoria === "MySQL" ? 'badge badge-pill badge-warning' : ''}
+                                ${itemTema.categoria === "Seguridad Informática" ? 'badge badge-pill badge-dark' : ''}
+                                ${itemTema.categoria === "JavaScript" ? 'badge badge-pill badge-success' : ''}
+                                ${itemTema.categoria === "PHP" ? 'badge badge-pill badge-primary' : ''}
+                        `}
+                     >{itemTema.categoria}</span>
+                 </td>
+              </tr>
+          )}
+      </tbody>
+  </table>
+    )
+  }
+
+  render() {
+    let contenidoTema = this.dibujarTema(this.state.listaTema);
+    return (
+      <>
+      <div className="container">
+        <div className="row">
+              <div className="col-2 col-sm-2 col-md-2">
+
+              </div>
+              <div className="col-8 col-sm-8 col-md-8">
+                <br></br>
+                <h4 id="titulo" className="text-center"> MÚLTIPLE CLASES DE CSS EN REACT </h4>
+                  <a href="https://github.com/PedroManuelJM" target="_blank">
+                    <h6 className="float-right"> <img src={github} width="12%" /> PedroManuelJM</h6>
+                  </a>
+                  {contenidoTema}
+              </div>
+
+        </div>
+
+      </div>
+     
+      </>
+    );
+
+  }
 }
-
-export default App;
---
+++ src/dataLocal/index.js
@@ -0,0 +1,9 @@
+export  const TemaData = [
+    {id: 1,tema: "Creando Proyecto en React ",categoria:"React"},
+    {id: 2,tema: "Creando Proyecto en Angular",categoria:"Angular"},
+    {id: 3,tema: "Validaciones con JavaScript",categoria:"JavaScript"},
+    {id: 4,tema: "Conexión con PHP hacia una API-REST ",categoria:"PHP"},
+    {id: 5,tema: "Creando el modelado de la base de datos ",categoria:"MySQL"},
+    {id: 6,tema: "Escaneo de Puertos - NMAP ",categoria:"Seguridad Informática"}
+
+]
